012d662098cf918057c606f5b812487c6e4c03e4
ShomaA0321 <shomaa2003@gmail.com>
2021-09-21 06:17:23 +0900

    updated

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
         <v-img alt="Logo" height="60" position="left" src="/static/img/logo.png" :transition="false" contain/>
       </router-link>
       <v-spacer/>
+      <router-link style="text-decoration: none" :to="{ path:'/', query: { src: 0 } }">
+        <v-btn text>
+          <span>Home</span>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+      </router-link>
       <router-link style="text-decoration: none" :to="{ path:'/history', query: { src: 0 } }">
         <v-btn text>
           <span>History</span>
@@ -12,7 +18,6 @@
         </v-btn>
       </router-link>
     </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,15 @@
   <v-container>
     <v-row>
       <v-col>
-        <p class="text-center text-h1 mt-2 mb-2">運動を選択してください</p>
+        <p class="text-center text-h1 ma-2">運動を選択してください</p>
       </v-col>
     </v-row>
+    <v-row class="ma-12"/>
     <v-row>
       <v-col v-for="(str, i) in ['腕の運動', '腹筋の運動', 'ももあげの運動']" :key="i">
         <router-link :to="{ path:'/measurement', query: { mode: i } }">
-          <v-btn color="primary" block x-large>
-            <p class="text-center text-body-1" v-text="str"/>
+          <v-btn color="primary" height="500" block x-large>
+            <p class="text-center text-h2" v-text="str"/>
           </v-btn>
         </router-link>
       </v-col>
